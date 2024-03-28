ee73d4c659b185d6413b2f5247bcabd8273ff6f9
josephclander <josephclander@gmail.com>
2024-03-28 15:50:07 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="display">
+        <p>Button has been clicked 0 times</p>
+      </div>
+      <button>Click me</button>
+    </>
+  );
+}
+
+export default App;
